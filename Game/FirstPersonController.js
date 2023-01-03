f3f2a6fcf7eb64ed99a3664006f8b2863a1620c0
TimJevsenak <tim.jevsenak@gmail.com>
2023-01-03 21:02:14 +0100

    coliders on map, lower jump

--- Game/FirstPersonController.js
+++ Game/FirstPersonController.js
@@ -179,7 +179,7 @@ export class FirstPersonController extends Node {
         //jump translation
         if(this.jump == true){
             this.jump_now =  Date.now()+1;
-            let height = 0.1*Math.sin(0.008*this.jump_now - 0.008*this.jump_start);
+            let height = 0.02*Math.sin(0.008*this.jump_now - 0.008*this.jump_start);
             //console.log(0.1*Math.sin(0.008*this.jump_now - 0.008*this.jump_start));
             if(height < 0){
                 this.jump_phase = -1;
