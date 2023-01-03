ba51df8108ada3e148f4d33554526d6b86b402ba
bmeznar <blaz.meznar.bm@gmail.com>
2023-01-03 20:49:57 +0100

    adding zombies

--- Game/Main.js
+++ Game/Main.js
@@ -21,6 +21,9 @@ class App extends Application {
         this.loader = new GLTFLoader();
         await this.loader.load('../Assets/map/lab_map.gltf');
 
+        //this.loader_zombie = new GLTFLoader();
+        //await this.loader_zombie.load('../Assets/zombie/zombie_v2.gltf');
+
         //FIRST PERSON CONTROLLER
         const gl = this.gl;
 
@@ -30,9 +33,14 @@ class App extends Application {
         this.startTime = this.time;
 
         //generating zombies
-        this.zombies = new Zombie();
+        //this.zombies = new Zombie();
+        this.zombies = new Array();
 
-        console.log(this.zombies);
+        for(let i = 0; i < 20; i++){
+            this.zombie = new Zombie();
+            this.zombies[i] = this.zombie;
+        }
+        //console.log(this.zombies);
 
 
         this.root = new Node();
@@ -52,8 +60,11 @@ class App extends Application {
         this.shadowCamera.aspect = 0.3;
         this.shadowCamera.near = 15;
         this.shadowCamera.far = 50;*/
+        //console.log(this.loader);
 
         this.scene = await this.loader.loadScene(this.loader.defaultScene);
+        //console.log(this.scene);
+        //for(let i = 0; i < this.zombies.)
 
         //this.camera = await this.loader.loadNode('Camera');
         //console.log(this.camera);
@@ -62,10 +73,22 @@ class App extends Application {
             throw new Error('Scene or Camera not present in glTF');
         }
 
+        let index = this.scene.nodes.length;
+        for(let i = 0; i < this.zombies.length; i++){
+            this.scene.nodes[index+i] = this.zombies[i].zombie_scene.nodes[0];
+        }
+        //console.log(this.scene);
+
+
         this.physics = new Physics(this.scene, this.controller);
 
         this.renderer = new Renderer(this.gl);
         this.renderer.prepareScene(this.scene);
+
+
+        //this.renderer.prepareScene(this.zombies.zombies_array[0]);
+        //this.renderer.prepareScene(this.scene_zombie);
+
         //this.renderer.prepareScene(this.scene_gun);
         this.resize();
     }
@@ -110,7 +133,10 @@ class App extends Application {
 
         this.controller.update(dt);
         this.physics.update(dt);
-        this.zombies.update();
+        
+        for(let i = 0; i < this.zombies.length; i++){
+            this.zombies[i].update();
+        }
     }
 
 }
--- Game/Zombie.js
+++ Game/Zombie.js
@@ -1,22 +1,42 @@
 import { vec3, mat4 } from '../../lib/gl-matrix-module.js';
 import { Node } from '../../common/engine/Node.js';
+import { GLTFLoader } from './GLTFLoader.js';
+import { Renderer } from './Renderer.js';
 
 export class Zombie {
-    constructor(scene) {
-    this.scene = scene;
-    this.zombies = new Node();
-
-        for(let i = 0; i < 20; i++){
-            let zombie = new Node();
-            let x = (Math.random() * 60) - 30;
-            let y = (Math.random() * 60) - 30;
-            zombie.translation = [x,1,y];
-            zombie.projection = mat4.create();
-            this.zombies.addChild(zombie);
-        }
+    constructor() {
+        this.make_zombie();
     }
+
+    async make_zombie(){
+        this.loader_zombie = new GLTFLoader();
+        await this.loader_zombie.load('../Assets/zombie/zombie_v2.gltf');
+        this.zombie_scene = await this.loader_zombie.loadScene(this.loader_zombie.defaultScene);
+
+        //this.zombies_array = new Array();
+        //let test = this.zombie_scene;
+        let x = (Math.random() * 80) - 40;
+        let y = (Math.random() * 80) - 40;
+        this.zombie_scene.nodes[0].translation = [x, 0, y];
+        //console.log(x+" "+y);
+        //this.zombies_array[i] = (test.nodes[0]);
+
+        /*for(let i = 0; i < 20; i++){
+            let test = this.zombie_scene;
+            let x = (Math.random() * 80) - 40;
+            let y = (Math.random() * 80) - 40;
+            test.nodes[0].translation = [x, 0, y];
+            //console.log(x+" "+y);
+            this.zombies_array[i] = (test.nodes[0]);
+        }*/
+
+    }
+
 
     update(){
         //spreminjanje koordinat zombijev
+        let x = this.zombie_scene.nodes[0].translation[0];
+        let y = this.zombie_scene.nodes[0].translation[2]+0.05;
+        this.zombie_scene.nodes[0].translation = [x, 0, y];
     }
 }
